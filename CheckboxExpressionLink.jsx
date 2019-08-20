dffc1760e03eb7de1da3180ff75239f144a0fe66
LK <laurens@epischcentrum.com>
2019-08-20 08:40:04 +0200

    Create  CheckboxExpressionLink.jsx

---
+++ CheckboxExpressionLink.jsx
@@ -0,0 +1,11 @@
+// Checkbox Expression Link - Created by Animoplex: www.animoplex.com
+// Link an effect or property to a checkbox control with an expression in After Effects.
+// Full Tutorial: https://www.youtube.com/watch?v=ggwCkj3sWZM
+
+src = effect("Checkbox Control")("Checkbox");
+if (src == true) {
+  // YOUR EXPRESSION HERE
+}
+else {
+  value;
+}
