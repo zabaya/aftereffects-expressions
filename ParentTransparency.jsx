45f2085cf3fb5f8e31922dfe893854a1be7146ed
LK <laurens@epischcentrum.com>
2019-08-20 12:59:23 +0200

    Create ParentTransparency.jsx

---
+++ ParentTransparency.jsx
@@ -0,0 +1,8 @@
+// Include transparancy in Parent
+
+// Quick fix 
+parent.transform.opacity
+
+// Cleaner version for if you remove the parent.
+if (hasParent == true) { parent.transform.opacity }
+else { value }
