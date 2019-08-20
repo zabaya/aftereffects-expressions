371f47db2d34f1b428c17007b09a981d7db9badd
LK <laurens@epischcentrum.com>
2019-08-20 08:43:17 +0200

    Update CheckboxExpressionLink.jsx

--- CheckboxExpressionLink.jsx
+++ CheckboxExpressionLink.jsx
@@ -1,6 +1,5 @@
 // Checkbox Expression Link
 // Link an effect or property to a checkbox control with an expression in After Effects.
-// Full Tutorial: https://www.youtube.com/watch?v=ggwCkj3sWZM
 
 src = effect("Checkbox Control")("Checkbox");
 if (src == true) {
