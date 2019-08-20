c1ea14a55346fa93ac7836ec9f67f693b62ff273
LK <laurens@epischcentrum.com>
2019-08-20 09:02:49 +0200

    Create WiggleAndLoop.jsx

---
+++ WiggleAndLoop.jsx
@@ -0,0 +1,7 @@
+// you can also put "pingpong" instead of "cycle" for your loop
+L = loopOut ("cycle" ) ; 
+
+// 3 is the times per second, 30 is the number of pixels it will vacillate
+W = wiggle ( 3, 30 ) ; 
+
+L + W - value;
