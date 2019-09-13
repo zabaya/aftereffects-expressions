d436d1e0b77f57b71c514a87d3a8cf02200615e4
LK <laurens@epischcentrum.com>
2019-09-13 10:40:08 +0200

    Create WiggleStartAndStopAtTime.jsx

---
+++ WiggleStartAndStopAtTime.jsx
@@ -0,0 +1,27 @@
+  // Wiggle start at 2 seconds:
+  
+  timeToStart = 2;
+    if (time > timeToStart){
+        wiggle(3,25);
+    } else {
+        value;
+    }
+  
+// Wiggle stops at 4 seconds:
+  
+  timeToStop = 4;
+    if (time > timeToStop){
+      value;
+    } else {
+      wiggle(3,25);
+    }
+
+// Wiggle start at 2 seconds and stops at 4 seconds:
+  
+  timeToStart = 2;
+  timeToStop = 4; 
+    if ((time > timeToStart) && (time < timeToStop)){
+        wiggle(3,25);
+    } else {
+        value;
+    }
