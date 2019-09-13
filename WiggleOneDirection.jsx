0681a7e567f418561c9df9d1fdb48b4fd022c86f
LK <laurens@epischcentrum.com>
2019-09-13 10:07:10 +0200

    Update WiggleOneDirection.jsx

--- WiggleOneDirection.jsx
+++ WiggleOneDirection.jsx
@@ -1,5 +1,17 @@
-x = transform.position[0];
-y = transform.position[1];
-w = wiggle(3, 200);
+// wiggle only in X (horizontal)
 
-[w[0], y];
+org=value;
+temp=wiggle (5,50);
+[temp[0],org[1]];
+
+// Shorthand (short version):
+[wiggle(5,50)[0],position[1]]
+
+// Wiggle only in Y (vertical)
+
+org=value;
+temp=wiggle (5,50);
+[org[0],temp[1]];
+
+// Shorthand (short version):
+[position[0],wiggle(5,50)[1]]
