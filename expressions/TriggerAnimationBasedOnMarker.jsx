5e1a4c7024f1a0e3c633ec3d1ebeeabe51dda85b
LK <laurens@epischcentrum.com>
2023-04-05 11:59:06 +0200

    Create TriggerAnimationBasedOnMarker.jsx

---
+++ expressions/TriggerAnimationBasedOnMarker.jsx
@@ -0,0 +1,27 @@
+// Copy this in the properties from the layer that you want to trigger based on markers
+
+L= thisLayer;
+
+n = 0;
+
+if (L.marker.numKeys > 0){
+
+n = L.marker.nearestKey(time).index;
+
+if (L.marker.key(n).time > time){
+
+n--;
+
+}
+
+}if (n == 0){
+
+valueAtTime(0);
+
+}else{
+
+t = time - L.marker.key(n).time;
+
+valueAtTime(t)
+
+}
