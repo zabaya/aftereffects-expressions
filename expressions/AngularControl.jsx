9d8bef9c3ef975583f38bd9032b7f6dc9d67c5c0
LK <laurens@epischcentrum.com>
2024-09-24 11:32:45 +0200

    Update AngularControl.jsx

--- expressions/AngularControl.jsx
+++ expressions/AngularControl.jsx
@@ -1,5 +1,7 @@
 // Add Slider & Angle control to the layer
 
+// Add below code to position of layer
+
 a = effect("Angle Control")("Angle");
 d = effect("Slider Control")("Slider");
 x = Math.cos(degreesToRadians(a));
